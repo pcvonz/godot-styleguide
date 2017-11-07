c6f31de494bfa681b6f071f2f8e059b1de408a68
pcvonz <pcvonz@gmail.com>
2017-11-06 17:55:12 -0800

    updating gh-pages

--- main.app.js
+++ main.app.js
@@ -73,6 +73,31 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__scss_style_scss___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__scss_style_scss__);
 
 
+// https://developer.mozilla.org/en-US/Add-ons/Code_snippets/QuerySelector
+function $ (selector, el) {
+     if (!el) {el = document;}
+     return el.querySelector(selector);
+}
+function $$ (selector, el) {
+     if (!el) {el = document;}
+     return el.querySelectorAll(selector);
+     // Note: the returned object is a NodeList.
+     // If you'd like to convert it to a Array for convenience, use this instead:
+     // return Array.prototype.slice.call(el.querySelectorAll(selector));
+}
+// generate nav
+
+let navList = $("#nav ul");
+let navItems = $$("h1.scaffold-menu");
+navItems.forEach(el => {
+  let li = document.createElement('li');
+  let a = document.createElement('a')
+  a.href = `#${el.id}`;
+  a.innerHTML = el.innerHTML;
+  li.appendChild(a);
+  navList.appendChild(li);
+});
+
 
 /***/ }),
 /* 1 */
