ba83b8c04df0296d13d45c820b4f2975b726d5ba
pcvonz <pcvonz@gmail.com>
2017-11-06 17:57:01 -0800

    Updating deploy

--- deploytogh.sh
+++ deploytogh.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-yarn build
-git checkout gh-pages
-mv dist/* .
-git commit -a -m "updating gh-pages"
-git push origin gh-pages
+if [[ `git status --porcelain` ]]; then
+  echo "Please stash or commit your changes"
+else
+  yarn build
+  git checkout gh-pages
+  mv dist/* .
+  git commit -a -m "updating gh-pages"
+  git push origin gh-pages
+  git checkout master
+fi
+
